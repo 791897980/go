6a27f096e882bcebb9de1163a60ef7a9a0742042
jinzhilai <jinzhilai@yy.com>
2022-05-25 04:04:47 +0900

    502502

--- go1/ch2/main.go
+++ go1/ch2/main.go
@@ -1,9 +1,32 @@
 package main
 
 import "fmt"
+import "strings"
 
 func main() {
-	var i int = 10
+	i := 10
+	var (
+		string1 string = "hello"
+		string2 string = "world"
+	)
 	fmt.Println(i)
-
+	fmt.Println("s1 + s2 = ", string1+string2)
+	var (
+		bool1 bool
+		a     string
+		b     int
+		c     uint
+		d     float32
+		e     float64
+	)
+	fmt.Println(bool1, a, b, c, d, e)
+	f := 1
+	g := "a"
+	h := true
+	i = 20
+	fmt.Println(f, g, h)
+	pi := &i
+	fmt.Println(*pi)
+	var string3 string = "o"
+	fmt.Println(strings.ContainsAny(string1, string3))
 }
